f00db79f56ea6dae045857d052586fa72aa82395
muhammadkhairielfataya <muhammadkhairielfataya@gmail.com>
2025-11-02 00:37:37 +0800

    Add files via upload

---
+++ mett2.py
@@ -0,0 +1,37 @@
+# Variabel adalah cara kita menyampaikan / mendeklarasikan nilai
+
+# memberikan nilai / asigment
+x = 10
+y = 6
+lebar =50
+
+print(" nilai dari x=", x)
+print(" nilai dari y=", y)
+print(" nilai dari lebar=", lebar)
+
+# penamaan fariabel
+jumlah_z =9 #lebih dari satu kata , maka hanya bisa digabungkan underscore
+bet99 = 100000
+#tidak boleh diawali dengan angka
+jumlahz = 10
+
+#pemanggilan kedua
+print(" nilai dari x=", x)
+x = 99
+print(" nilai dari x=", x)
+
+
+# pemanggilan indireck
+a = x
+b = lebar
+print(" nilai dari x=", x)
+print(" nilai dari b=", b)
+
+# tugas :
+# nama saya ={Muhammad Khairiel Fataya}
+# nim saya = 25241074
+# buat program agar muncul dilayar
+
+# jawab
+print ("nama saya adalah = Muhammad Khairiel Fataya")
+print ("nim saya = 25241074")
