0f985fb88b1d8553767edf158444d23839249b7b
muhammadkhairielfataya <muhammadkhairielfataya@gmail.com>
2025-11-15 23:19:07 +0800

    Add files via upload

---
+++ mett4.py
@@ -0,0 +1,56 @@
+# mengambil imput dari user
+
+nama = input("masukan nama anda")
+print("selamat datang", nama)
+print("nilai dari nama", nama)
+print(type(nama))
+
+bil_int = int (input("masukan bilangan anda"))
+print("bilangan anda adalah", bil_int)
+print("nilai dari bil_int", bil_int)
+print(type(bil_int))
+
+bil_float = float (input("masukan bilangan anda"))
+print("bilangan anda adalah", bil_float)
+print("nilai dari bil_float", bil_float)
+print(type(bil_float))
+
+bil_bool = bool (input("masukan bilangan anda"))
+print("bilangan anda adalah", bil_bool)
+print("nilai dari bil_bool", bil_bool)
+print(type(bil_bool))
+
+# buatlah program dengan output sebagai berikut
+# masukan nama anda :
+# masukan umur anda :
+# nama anda adalah (nama)
+# type data dari nama anada adalah (type data dari nama)
+# umur anda adalah (umur)
+# type data dari umur anda adalah(type data dari umur)
+
+# mengambil input data dari user 
+
+nama = input ("masukkan nama anda ")
+print("selamat datang ", nama)
+print("nilai dari nama", nama)
+print(type(nama))
+
+bil_int = int(input("masukan umur anda "))
+print("umur anda adalah ", bil_int)
+print("nilai dari bil_int ", bil_int)
+print(type(bil_int))
+
+bil_float = float(input("masukkan bilangan anda "))
+print("bilangan anda adalah ", bil_float)
+print("nilai dari bilangan bil_float", bil_float)
+print(type(bil_float))
+
+bil_bool = bool(input("masukkan bilangan anda "))
+print("bilangan anda adlah bil_bool ", bil_bool)
+print("nilai dari bil_bool ", bil_bool)
+print(type(bil_bool))
+
+
+
+
+
