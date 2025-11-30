c64a6b898d5f795ccbf94f737bfefdddcd019e94
muhammadkhairielfataya <muhammadkhairielfataya@gmail.com>
2025-11-30 20:27:25 +0800

    Add files via upload

---
+++ mett5.py
@@ -0,0 +1,58 @@
+#oprasi aritmatika (x : + -)
+
+# penjumlahan
+a = 9
+b = 3
+hasil = a + b 
+print (a, "+", b, "=", hasil)
+
+# pengurangan -
+# perkalian *
+# pembagian /
+# perpangkatan **
+# modulus
+# floor divison
+
+# pengurangan 
+a = 9
+b = 3
+hasil = a - b 
+print (a, "-", b, "=", hasil)
+
+# perkalian
+a = 9
+b = 3
+hasil = a * b 
+print (a, "*", b, "=", hasil)
+
+# pembagian
+a = 9
+b = 3
+hasil = a / b 
+print (a, "/", b, "=", hasil)
+
+# perpangkatan
+a = 9
+b = 3
+hasil = a ** b 
+print (a, "**", b, "=", hasil)
+
+# modulus/ adalah sisa bagi
+a = 9
+b = 3
+hasil = a % b 
+print (a, "modulus", b, "=", hasil)
+
+# floor division/ hasilnya yang selalu di bulatkan
+a = 9
+b = 3
+hasil = a // b 
+print (a, "floor div", b, "=", hasil)
+
+
+# yang dapat kita ambil di aritmatika
+# oprasi prioritas
+# 1. ()
+# 2. pangkat akar
+# 3. x :
+# 4. + -
